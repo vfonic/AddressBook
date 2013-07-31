b71eb090ef1d67848b4ee8317cba020ced7c32e7
Viktor Fonic <viktor.fonic@gmail.com>
2013-08-01 01:09:45 +0200

    Add ProfileActivity

---
+++ AddressBook/src/main/java/com/viktorfonic/addressbook/activity/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.viktorfonic.addressbook.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.viktorfonic.addressbook.R;
+
+/**
+ * Created by viktorfonic on 01.08.2013.
+ */
+public class ProfileActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_contact_activity);
+    }
+}
