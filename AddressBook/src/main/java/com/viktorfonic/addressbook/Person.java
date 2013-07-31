41bf0e98391b53fdce6b567999aa279d37f46948
Viktor Fonic <viktor.fonic@gmail.com>
2013-07-31 23:18:19 +0200

    Override toString in Person

--- AddressBook/src/main/java/com/viktorfonic/addressbook/Person.java
+++ AddressBook/src/main/java/com/viktorfonic/addressbook/Person.java
@@ -35,4 +35,9 @@ public class Person {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return name + " (" + phone + ", " + email + ")";
+    }
 }
