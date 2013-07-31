93ed1bc3440eebc18b0d07bc2cfb9f6d000afb68
Viktor Fonic <viktor.fonic@gmail.com>
2013-07-31 23:52:28 +0200

    Use predefined two line layout for ListView

--- AddressBook/src/main/java/com/viktorfonic/addressbook/HomeActivity.java
+++ AddressBook/src/main/java/com/viktorfonic/addressbook/HomeActivity.java
@@ -3,10 +3,11 @@ package com.viktorfonic.addressbook;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HomeActivity extends Activity {
 
@@ -15,19 +16,28 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
-        
-        ArrayList<Person> people = createDummyPeople();
-        
+
+        ArrayList<HashMap<String, String>> people = createDummyPeople();
+
         ListView listView_Contacts = (ListView) findViewById(R.id.listView_contacts);
-        listView_Contacts.setAdapter(new ArrayAdapter<Person>(this, android.R.layout.simple_list_item_1, people));
+        SimpleAdapter adapter = new SimpleAdapter(this, people, android.R.layout.two_line_list_item, new String[]{"full_name", "contact"}, new int[]{android.R.id.text1, android.R.id.text2});
+        listView_Contacts.setAdapter(adapter);
+//        listView_Contacts.setAdapter(new ArrayAdapter<Person>(this, android.R.layout.simple_list_item_1, people));
     }
 
-    private ArrayList<Person> createDummyPeople() {
-        ArrayList<Person> people = new ArrayList<Person>();
-        people.add(new Person("Pero", "098 181 24 33", "pero.peric@gmail.com"));
-        people.add(new Person("Ivo", "091 161 24 33", "ivo.ivic@gmail.com"));
-        people.add(new Person("Mate", "095 171 24 33", "mate.matic@hotmail.com"));
+    private ArrayList<HashMap<String, String>> createDummyPeople() {
+        ArrayList<HashMap<String, String>> people = new ArrayList<HashMap<String, String>>();
+        people.add(createMapFromPerson(new Person("Pero Perić", "098 181 24 33", "pero.peric@gmail.com")));
+        people.add(createMapFromPerson(new Person("Ivo Ivić", "098 181 24 33", "ivo.peric@gmail.com")));
+        people.add(createMapFromPerson(new Person("Marko Marković", "098 181 24 33", "marko.peric@gmail.com")));
         return people;
+    }
+
+    private HashMap<String, String> createMapFromPerson(Person p) {
+        HashMap<String, String> person = new HashMap<String, String>();
+        person.put("full_name", p.getName());
+        person.put("contact", p.getPhone() + ", " + p.getEmail());
+        return person;
     }
 
     @Override
