9fbf7973d46e1c0bf9dd78fed32a3b1d0e0b3d7f
Viktor Fonic <viktor.fonic@gmail.com>
2013-07-31 22:25:09 +0200

    Show dummy people in ListView

--- AddressBook/src/main/java/com/viktorfonic/addressbook/HomeActivity.java
+++ AddressBook/src/main/java/com/viktorfonic/addressbook/HomeActivity.java
@@ -1,17 +1,34 @@
 package com.viktorfonic.addressbook;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+        
+        ArrayList<Person> people = createDummyPeople();
+        
+        ListView listView_Contacts = (ListView) findViewById(R.id.listView_contacts);
+        listView_Contacts.setAdapter(new ArrayAdapter<Person>(this, android.R.layout.simple_list_item_1, people));
     }
 
+    private ArrayList<Person> createDummyPeople() {
+        ArrayList<Person> people = new ArrayList<Person>();
+        people.add(new Person("Pero", "098 181 24 33", "pero.peric@gmail.com"));
+        people.add(new Person("Ivo", "091 161 24 33", "ivo.ivic@gmail.com"));
+        people.add(new Person("Mate", "095 171 24 33", "mate.matic@hotmail.com"));
+        return people;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +36,5 @@ public class HomeActivity extends Activity {
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
-    
+
 }
