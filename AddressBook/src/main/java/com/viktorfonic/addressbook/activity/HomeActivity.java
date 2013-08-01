99422d15f1cef6c89f76039c71b20cba25260da2
Viktor Fonic <viktor.fonic@gmail.com>
2013-08-01 13:25:43 +0200

    Send data to ProfileActivity and show it when activity starts

--- AddressBook/src/main/java/com/viktorfonic/addressbook/activity/HomeActivity.java
+++ AddressBook/src/main/java/com/viktorfonic/addressbook/activity/HomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -13,30 +14,33 @@ import com.viktorfonic.addressbook.R;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements AdapterView.OnItemClickListener {
 
-    private static ArrayList<HashMap<String, String>> contacts;
+    private static ArrayList<HashMap<String, String>> contactsHash;
+    private static ArrayList<Contact> contacts;
     private SimpleAdapter adapter;
-    private ListView listView_Contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
-        contacts = createDummyPeople();
+        contactsHash = new ArrayList<HashMap<String, String>>();
+        createDummyPeople();
 
-        listView_Contacts = (ListView) findViewById(R.id.listView_contacts);
-        adapter = new SimpleAdapter(this, contacts, android.R.layout.two_line_list_item, new String[]{"full_name", "contact"}, new int[]{android.R.id.text1, android.R.id.text2});
+        ListView listView_Contacts = (ListView) findViewById(R.id.listView_contacts);
+        adapter = new SimpleAdapter(this, contactsHash, android.R.layout.two_line_list_item, new String[]{"full_name", "contact"}, new int[]{android.R.id.text1, android.R.id.text2});
         listView_Contacts.setAdapter(adapter);
+        listView_Contacts.setOnItemClickListener(this);
     }
 
-    private ArrayList<HashMap<String, String>> createDummyPeople() {
-        ArrayList<HashMap<String, String>> contacts = new ArrayList<HashMap<String, String>>();
-        contacts.add(createMapFromContact(new Contact("Pero Perić", "098 181 24 33", "pero.peric@gmail.com")));
-        contacts.add(createMapFromContact(new Contact("Ivo Ivić", "098 181 24 33", "ivo.peric@gmail.com")));
-        contacts.add(createMapFromContact(new Contact("Marko Marković", "098 181 24 33", "marko.peric@gmail.com")));
-        return contacts;
+    private void createDummyPeople() {
+        contacts = new ArrayList<Contact>();
+        contacts.add(new Contact("Pero Perić", "098 181 24 33", "pero.peric@gmail.com"));
+        contacts.add(new Contact("Ivo Ivić", "098 181 24 33", "ivo.peric@gmail.com"));
+        contacts.add(new Contact("Marko Marković", "098 181 24 33", "marko.peric@gmail.com"));
+        for (Contact c : contacts)
+            contactsHash.add(createMapFromContact(c));
     }
 
     private HashMap<String, String> createMapFromContact(Contact p) {
@@ -61,11 +65,20 @@ public class HomeActivity extends Activity {
                     String phone = data.getStringExtra(Contact.PHONE);
                     String email = data.getStringExtra(Contact.EMAIL);
                     Contact c = new Contact(name, phone, email);
-                    contacts.add(createMapFromContact(c));
-                    adapter = new SimpleAdapter(this, contacts, android.R.layout.two_line_list_item, new String[]{"full_name", "contact"}, new int[]{android.R.id.text1, android.R.id.text2});
-                    listView_Contacts.setAdapter(adapter);
+                    contactsHash.add(createMapFromContact(c));
+                    contacts.add(c);
                     adapter.notifyDataSetChanged();
                     break;
             }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Contact c = contacts.get(position);
+        Intent i = new Intent(this, ProfileActivity.class);
+        i.putExtra(Contact.NAME, c.getName());
+        i.putExtra(Contact.PHONE, c.getPhone());
+        i.putExtra(Contact.EMAIL, c.getEmail());
+        startActivity(i);
     }
 }
--- AddressBook/src/main/java/com/viktorfonic/addressbook/activity/ProfileActivity.java
+++ AddressBook/src/main/java/com/viktorfonic/addressbook/activity/ProfileActivity.java
@@ -1,8 +1,11 @@
 package com.viktorfonic.addressbook.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.viktorfonic.addressbook.Contact;
 import com.viktorfonic.addressbook.R;
 
 /**
@@ -13,5 +16,13 @@ public class ProfileActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_contact_activity);
+
+        Intent intent = getIntent();
+        TextView fullName_tv = (TextView) findViewById(R.id.full_name);
+        TextView phone_tv = (TextView) findViewById(R.id.phone);
+        TextView email_tv = (TextView) findViewById(R.id.email);
+        fullName_tv.setText(intent.getStringExtra(Contact.NAME));
+        phone_tv.setText(intent.getStringExtra(Contact.PHONE));
+        email_tv.setText(intent.getStringExtra(Contact.EMAIL));
     }
 }
