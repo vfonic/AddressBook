84f4877f907ddbfe9f916142d38fa0566f21c9d4
Viktor Fonic <viktor.fonic@gmail.com>
2013-08-20 16:57:44 +0200

    Fetch Contacts from phone using Loader

--- AddressBook/src/main/java/com/viktorfonic/addressbook/activity/HomeActivity.java
+++ AddressBook/src/main/java/com/viktorfonic/addressbook/activity/HomeActivity.java
@@ -1,83 +1,43 @@
 package com.viktorfonic.addressbook.activity;
 
 import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
 
-import com.google.gson.Gson;
 import com.viktorfonic.addressbook.Contact;
-import com.viktorfonic.addressbook.People;
 import com.viktorfonic.addressbook.R;
-import com.viktorfonic.addressbook.Root;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
+public class HomeActivity extends Activity implements AdapterView.OnItemClickListener, LoaderManager.LoaderCallbacks<Cursor> {
 
-public class HomeActivity extends Activity implements AdapterView.OnItemClickListener {
-
-    private static ArrayList<HashMap<String, String>> contactsHash;
-    private static ArrayList<Contact> contacts;
-    private SimpleAdapter adapter;
+    public static final String[] PROJECTION = {ContactsContract.Data._ID, ContactsContract.Data.DISPLAY_NAME,
+            ContactsContract.Data.PHOTO_THUMBNAIL_URI};
+    public static final String SELECTION = "((" + ContactsContract.Data.DISPLAY_NAME + " NOTNULL) AND ("
+            + ContactsContract.Data.DISPLAY_NAME + " != ''))";
+    private SimpleCursorAdapter mAdapter;
+    private Loader<Object> loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
-        contactsHash = new ArrayList<HashMap<String, String>>();
-//        createDummyPeople();
-        loadPeopleFromJSON();
-
-        ListView listView_Contacts = (ListView) findViewById(R.id.listView_contacts);
-        adapter = new SimpleAdapter(this, contactsHash, android.R.layout.two_line_list_item, new String[]{"full_name", "contact"}, new int[]{android.R.id.text1, android.R.id.text2});
-        listView_Contacts.setAdapter(adapter);
-        listView_Contacts.setOnItemClickListener(this);
-    }
-
-    private void loadPeopleFromJSON() {
-        try {
-            InputStream is = this.getAssets().open("people.json");
-
-            int ch;
-            StringBuilder sb = new StringBuilder();
-            while ((ch = is.read()) != -1)
-                sb.append((char) ch);
-
-            String json = sb.toString();
-
-            contacts = new ArrayList<Contact>();
-            Gson gson = new Gson();
-            Root root = gson.fromJson(json, Root.class);
-            for (People p :root.getPeople()) {
-                contacts.add(p.getPerson());
-                contactsHash.add(createMapFromContact(p.getPerson()));
-            }
-        } catch (IOException e) {
-            Log.e(HomeActivity.class.getName(), e.getMessage());
-        }
-    }
-
-    private void createDummyPeople() {
-        contacts = new ArrayList<Contact>();
-        contacts.add(new Contact("Pero Perić", "098 181 24 33", "pero.peric@gmail.com"));
-        contacts.add(new Contact("Ivo Ivić", "098 181 24 33", "ivo.peric@gmail.com"));
-        contacts.add(new Contact("Marko Marković", "098 181 24 33", "marko.peric@gmail.com"));
-        for (Contact c : contacts)
-            contactsHash.add(createMapFromContact(c));
-    }
-
-    private HashMap<String, String> createMapFromContact(Contact p) {
-        HashMap<String, String> person = new HashMap<String, String>();
-        person.put("full_name", p.getName());
-        person.put("contact", p.getPhone() + ", " + p.getEmail());
-        return person;
+        String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME};
+        int[] toViews = {android.R.id.text1};
+        ListView listView = (ListView) findViewById(R.id.listView_contacts);
+//        mAdapter = new SimpleCursorAdapter(this, contactsHash, android.R.layout.two_line_list_item, new String[]{"full_name", "contact"}, new int[]{android.R.id.text1, android.R.id.text2});
+        mAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, fromColumns, toViews, 0);
+        listView.setAdapter(mAdapter);
+//        listView_Contacts.setOnItemClickListener(this);
+        getLoaderManager().initLoader(0, null, this);
     }
 
     @SuppressWarnings("unused")
@@ -95,20 +55,34 @@ public class HomeActivity extends Activity implements AdapterView.OnItemClickLis
                     String phone = data.getStringExtra(Contact.PHONE);
                     String email = data.getStringExtra(Contact.EMAIL);
                     Contact c = new Contact(name, phone, email);
-                    contactsHash.add(createMapFromContact(c));
-                    contacts.add(c);
-                    adapter.notifyDataSetChanged();
+//                    contacts.add(c);
+                    mAdapter.notifyDataSetChanged();
                     break;
             }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Contact c = contacts.get(position);
+//        Contact c = contacts.get(position);
         Intent i = new Intent(this, ProfileActivity.class);
-        i.putExtra(Contact.NAME, c.getName());
-        i.putExtra(Contact.PHONE, c.getPhone());
-        i.putExtra(Contact.EMAIL, c.getEmail());
+//        i.putExtra(Contact.NAME, c.getName());
+//        i.putExtra(Contact.PHONE, c.getPhone());
+//        i.putExtra(Contact.EMAIL, c.getEmail());
         startActivity(i);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, ContactsContract.Data.CONTENT_URI, PROJECTION, SELECTION, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
     }
 }
